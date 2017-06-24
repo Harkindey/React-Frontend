58cddb4f07619377406a44cf11bb4dce02483ad5
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-06-24 14:16:02 +0100

    Webpack and Babel Config

---
+++ app/index.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('./index.css');
+
+//Component may have state
+//Component may have lifecycle event
+//Component WILL have UI
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />,document.getElementById('app'));
