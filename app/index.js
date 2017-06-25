9732645f1c2bebcefb090239de6e9962b494ac49
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-06-25 22:50:38 +0100

    The popular component rendered as a stateless functiononal component

--- app/index.js
+++ app/index.js
@@ -2,19 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 require('./index.css');
+var App = require('./components/App');
 
 //Component may have state
 //Component may have lifecycle event
 //Component WILL have UI
 
-class App extends React.Component {
-  render(){
-    return(
-      <div>
-        Hello React Training
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App />,document.getElementById('app'));
